1a9f974f511975dd9da6c04a75060a5e782367e9
apobekiaris <apostolis.bekiaris@gmail.com>
2019-01-16 07:47:35 +0200

    Switch to XAF namespace #300

--- tools/Build/PushNugets.ps1
+++ tools/Build/PushNugets.ps1
@@ -4,7 +4,7 @@ param(
     [parameter()]
     $source="https://xpandnugetserver.azurewebsites.net/nuget",
     [parameter()]
-    $criteria="DevExpress.XAF*",
+    $criteria="Xpand.XAF*",
     [parameter()]
     $apiKey=$null
 )
@@ -26,11 +26,11 @@ $packages=$nugetResult|ForEach-Object{
 
 Get-ChildItem *.nupkg -Recurse|ForEach-Object{
     $localPackageName=[System.IO.Path]::GetFileNameWithoutExtension($_)
-    $r=New-Object System.Text.RegularExpressions.Regex("[\d]{2}\.[\d]{1}\.[\d]*(\.[\d]*)?")
+    $r=New-Object System.Text.RegularExpressions.Regex("[\d]{1,2}\.[\d]{1}\.[\d]*(\.[\d]*)?")
     $localPackageVersion=$r.Match($localPackageName).Value
     $localPackageName=$localPackageName.Replace($localPackageVersion,"").Trim(".")
     $package=$packages|Where-Object{$_.name -eq $localPackageName  }
-    if ($package.Version -ne $localPackageVersion){
+    if (!$package -and $package.Version -ne $localPackageVersion){
         "Pushing $($_.FullName)"
         & $nugetExe push $_.FullName -source $source -ApiKey $apikey
     }
